fbd9ab6c215f3053bd1e678e410f8ffc1253dd3d
Dominic Canora <dominiccanora@gmail.com>
2024-07-29 12:28:11 -0400

    Oops

--- app/models.py
+++ app/models.py
@@ -26,15 +26,16 @@ class CanvasToken(models.Model):
     person_id = models.CharField(max_length=100)
 
     def refresh_and_delete(self):
-        resp = requests.post('https://lhps.instructure.com/login/oauth2/token', {
-            "code": self.refresh_token,
-            "grant_type": "authorization_code",
+        resp = requests.post("https://lhps.instructure.com/login/oauth2/token", {
+            "grant_type": "refresh_token",
             "client_id": "75360000000000229",
             "client_secret": os.getenv("CANVAS_CLIENT_SECRET"),
-            "redirect_uri": "https://dash.canora.us/canvas/oauth"
+            "redirect_uri": "https://dash.canora.us/canvas/oauth",
+            "refresh_token": self.refresh_token
         })
 
         resp = resp.json()
+        print(resp)
 
         requests.delete(
             'https://lhps.instructure.com/login/oauth2/token',

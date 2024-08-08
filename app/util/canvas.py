08d0e1e2b397729b83213d043f668ff85bac3a69
Dominic Canora <dominiccanora@gmail.com>
2024-08-08 10:26:46 -0400

    I figured it out.

--- app/util/canvas.py
+++ app/util/canvas.py
@@ -1,16 +1,39 @@
+import os
 import re
+from datetime import timedelta
 
 import requests
 from dateutil.parser import parse
 from django.http.response import HttpResponseForbidden
+from django.utils import timezone
 
 
 def get_name(request, course_id):
     return dict(request.session['names']).get(course_id, "Unknown Course")
 
 
+def do_refresh_if_needed(request):
+    if timezone.now().timestamp() >= request.session['expires']:
+        resp = requests.post("https://lhps.instructure.com/login/oauth2/token", {
+            "grant_type": "refresh_token",
+            "client_id": "75360000000000229",
+            "client_secret": os.getenv("CANVAS_CLIENT_SECRET"),
+            "redirect_uri": "https://dash.canora.us/canvas/oauth",
+            "refresh_token": request.session['refresh_token']
+        })
+
+        resp = resp.json()
+
+        request.session['access_token'] = resp['access_token']
+        request.session['expires'] = (
+                timezone.now() + timedelta(seconds=int(resp['expires_in']) - 60)).timestamp()
+        request.session.save()
+
+
 def get_activity_stream(request):
     try:
+        do_refresh_if_needed(request)
+
         resp = requests.get("https://lhps.instructure.com/api/v1/users/self/activity_stream", headers={
             "Authorization": f"Bearer {request.session['access_token']}"
         })
@@ -23,12 +46,6 @@ def get_activity_stream(request):
 
         return resp
     except TypeError:
-        try:
-            print("Logging out")
-            print(resp)
-        except:
-            pass
-
         del request.session['access_token']
         del request.session['refresh_token']
 
@@ -39,6 +56,8 @@ def get_activity_stream(request):
 
 def get_todo(request):
     try:
+        do_refresh_if_needed(request)
+
         resp = requests.get("https://lhps.instructure.com/api/v1/users/self/todo", headers={
             "Authorization": f"Bearer {request.session['access_token']}"
         })
@@ -51,12 +70,6 @@ def get_todo(request):
 
         return resp
     except TypeError:
-        try:
-            print("Logging out")
-            print(resp)
-        except:
-            pass
-
         del request.session['access_token']
         del request.session['refresh_token']
 
@@ -67,6 +80,8 @@ def get_todo(request):
 
 def get_grades_and_set_names(request):
     try:
+        do_refresh_if_needed(request)
+
         resp = requests.get("https://lhps.instructure.com/api/v1/courses", headers={
             "Authorization": f"Bearer {request.session['access_token']}"
         }, params={
@@ -83,12 +98,6 @@ def get_grades_and_set_names(request):
 
         return resp
     except TypeError:
-        try:
-            print("Logging out")
-            print(resp)
-        except:
-            pass
-
         del request.session['access_token']
         del request.session['refresh_token']
 
--- app/views.py
+++ app/views.py
@@ -23,21 +23,7 @@ def index(request):
         sec_dark = color.is_dark(secondary)
 
         if 'access_token' in request.session:
-            if timezone.now().timestamp() >= request.session['expires']:
-                resp = requests.post("https://lhps.instructure.com/login/oauth2/token", {
-                    "grant_type": "refresh_token",
-                    "client_id": "75360000000000229",
-                    "client_secret": os.getenv("CANVAS_CLIENT_SECRET"),
-                    "redirect_uri": "https://dash.canora.us/canvas/oauth",
-                    "refresh_token": request.session['refresh_token']
-                })
-
-                resp = resp.json()
-
-                request.session['access_token'] = resp['access_token']
-                request.session['expires'] = (
-                        timezone.now() + timedelta(seconds=int(resp['expires_in']) - 60)).timestamp()
-                request.session.save()
+            canvas.do_refresh_if_needed(request)
 
             return render(request, "app/index.html", {
                 "primary": primary,
